198ba5ff12ca67cdc08735a358b2e9386b7e76c9
s18321 <61875317+s18321@users.noreply.github.com>
2020-10-31 17:53:19 +0100

    initial commit

---
+++ task2/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var url = @"https://www.pja.edu.pl";
+            using (var httpClient = new HttpClient()) // var assignes the variable as right
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+
+                    var content = await response.Content.ReadAsStringAsync();
+
+
+
+
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var matches = regex.Matches(content); //Match only find one occurence and matches find more of them
+
+
+
+                    foreach (var match in matches)
+                    {
+                        Console.WriteLine(match.ToString());
+                    }
+
+                }
+            }
+        }
+    }
+}
