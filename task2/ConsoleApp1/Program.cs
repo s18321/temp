9f334d19b467642a7104f6779f93e056358b481a
s18321 <61875317+s18321@users.noreply.github.com>
2020-11-01 12:41:45 +0100

    commit that should go to branch additonal_requirements

--- task2/ConsoleApp1/Program.cs
+++ task2/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,31 +11,91 @@ namespace ConsoleApp1
     {
         public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var url = @"https://www.pja.edu.pl";
-            using (var httpClient = new HttpClient()) // var assignes the variable as right
+           // args[0] = new string("https://www.pja.edu.pl");
+            var listOfEmails = new List<string>();
+            try
             {
-                using (var response = await httpClient.GetAsync(url))
+                using (var httpClient = new HttpClient())
                 {
 
-                    var content = await response.Content.ReadAsStringAsync();
-
-
-
-
-                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                    var matches = regex.Matches(content); //Match only find one occurence and matches find more of them
-
-
-
-                    foreach (var match in matches)
+                    using (var response = await httpClient.GetAsync(args[0])) // using(){} - automatically manages resources - like finally in Java
                     {
-                        Console.WriteLine(match.ToString());
+                        var content = await response.Content.ReadAsStringAsync();
+
+                        var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                        var matches = regex.Matches(content);  //Match only find one occurence and matches find more of them
+
+                        foreach (var match in matches)
+                        {
+                            //Console.WriteLine(match.ToString());
+                            if (!listOfEmails.Contains(match.ToString()))
+                            {
+                                listOfEmails.Add(match.ToString());
+                            }
+                        }
+
+                        if (listOfEmails.Count == 0)
+                        {
+                            Console.WriteLine("No email addresses found");
+                        }
+                        foreach (var email in listOfEmails)
+                        {
+                            Console.WriteLine(email);
+                        }
+
+
                     }
+                 
 
                 }
             }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Index out of range");
+            }
+            catch (System.Net.Http.HttpRequestException ex)
+            {
+                Console.WriteLine("Error while downloading the page");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("ArgumentNullException");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("ArgumentException");
+            }
         }
+
+
+        /*
+        Console.WriteLine("Hello World!");
+        var url = @"https://www.pja.edu.pl";
+        using (var httpClient = new HttpClient()) // var assignes the variable as right
+        {
+            using (var response = await httpClient.GetAsync(url))
+            {
+
+                var content = await response.Content.ReadAsStringAsync();
+
+
+
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(content); //Match only find one occurence and matches find more of them
+
+                Console.WriteLine("Hello World!");
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                    Console.WriteLine("Hello World!");
+                }
+
+            }
+        }*/
     }
 }
+
